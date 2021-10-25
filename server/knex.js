17ddcec70c656efefdf350e2edabdd5e3eb25b76
mAtencio27 <mAtencio27@hotmail.com>
2021-10-25 12:18:53 +0900

    fixed the naming error with local. Heroku config comlete

--- server/knex.js
+++ server/knex.js
@@ -4,7 +4,8 @@ const db = knex({
     client:"pg",
     connection:
         process.env.DATABASE_URL ||
-        `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@127.0.0.1:5432/jjot`,
+                                                                                    //dbname
+        `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@127.0.0.1:5432/jot`,
     searchPath: "public",
 });
 
