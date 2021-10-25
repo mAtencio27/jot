6a544140ed68cd47c082cf2aa01d0794eeb17230
mAtencio27 <mAtencio27@hotmail.com>
2021-10-25 14:11:02 +0900

    endpoints completed

--- server/app.js
+++ server/app.js
@@ -21,10 +21,45 @@ app.use(express.static(path.resolve(__dirname, "..", "build")));
 app.get("/api/hello", async(req,res) => {
    res.json("hello this app is jot")
 })
-
+//GET ALL NOTES
 app.get("/api/allnotes", async(req,res) => {
     const notes = await db.table("notes")
     res.json(notes)
+})
+//GET ONE NOTE
+app.get("/api/onenote", async(req,res) => {
+    const notes = await db.table("notes")
+    const noteId = parseInt(req.body.id)
+    const singleNote = notes.filter((note)=> noteId === note.id)
+    res.json(singleNote)
+})
+//POST A NOTE
+app.post("/api/add", async(req,res) => {
+    const addNote = req.body.note
+    const obj = {"note": addNote}
+    await db.table("notes").insert(obj)
+    res.json(`added to Notes!`)
+})
+//DELETE A NOTE 
+app.delete("/api/delete", async(req,res) => {
+    const deleteId = parseInt(req.body.id)
+    const notes = await db.table("notes")
+    const foundItem = notes.filter((note) => deleteId === note.id)
+    await db.table("notes").where("id",foundItem[0].id).delete()
+    res.json(`Note with ID number ${deleteId} has been deleted`)
+})
+//EDIT A NOTE
+//{"id":"9", "note":"lets patch this bad boy note up. ITS WORKING!!!"}
+app.patch("/api/patch", async(req,res) => {
+    const patchId = parseInt(req.body.id)
+    const patchNote = req.body.note
+    //console.log(patchId, patchNote) OK
+    const notes = await db.table("notes")
+    //console.log(notes) OK
+    const editNote = notes.filter((note) => patchId === note.id)
+    //console.log(editNote)
+    await db.table("notes").where("id", editNote[0].id).update({note:patchNote})
+    res.json("all patched up")
 })
 
 /*************************************************************************** */
